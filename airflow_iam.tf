e800e4b141247997eba2cde1393d3d992160e687
DSkrzypiec <damian.j.skrzypiec@gmail.com>
2023-03-27 18:16:00 +0200

    Add IAM airflow exec role definition

---
+++ airflow_iam.tf
@@ -0,0 +1,135 @@
+resource "aws_iam_role" "this" {
+  name               = "airflow-mwaa-${var.environment_name}-execution-role"
+  assume_role_policy = data.aws_iam_policy_document.assume.json
+  tags = {
+    Name = "mwaa-${var.environment_name}-iam-role"
+  }
+}
+
+resource "aws_iam_role_policy" "this" {
+  name   = "mwaa-${var.environment_name}-execution-policy"
+  policy = data.aws_iam_policy_document.this.json
+  role   = aws_iam_role.this.id
+}
+
+data "aws_iam_policy_document" "assume" {
+  version = "2012-10-17"
+  statement {
+    effect = "Allow"
+    principals {
+      identifiers = [
+        "airflow-env.amazonaws.com",
+        "airflow.amazonaws.com"
+      ]
+      type = "Service"
+    }
+    actions = [
+      "sts:AssumeRole"
+    ]
+  }
+}
+
+data "aws_iam_policy_document" "base" {
+  version = "2012-10-17"
+  statement {
+    effect = "Allow"
+    actions = [
+      "airflow:PublishMetrics"
+    ]
+    resources = [
+      "arn:aws:airflow:${var.region}:${var.account_id}:environment/${var.environment_name}"
+    ]
+  }
+  statement {
+    effect  = "Deny"
+    actions = ["s3:ListAllMyBuckets"]
+    resources = [
+      aws_s3_bucket.airflow_dags.arn,
+      "${aws_s3_bucket.airflow_dags.arn}/*",
+    ]
+  }
+  statement {
+    effect = "Allow"
+    actions = [
+      "s3:GetObject*",
+      "s3:GetBucket*",
+      "s3:List*"
+    ]
+    resources = [
+      aws_s3_bucket.airflow_dags.arn,
+      "${aws_s3_bucket.airflow_dags.arn}/*",
+    ]
+  }
+  statement {
+    effect = "Allow"
+    actions = [
+      "s3:GetAccountPublicAccessBlock"
+    ]
+    resources = ["*"]
+  }
+  statement {
+    effect = "Allow"
+    actions = [
+      "logs:CreateLogStream",
+      "logs:CreateLogGroup",
+      "logs:PutLogEvents",
+      "logs:GetLogEvents",
+      "logs:GetLogRecord",
+      "logs:GetLogGroupFields",
+      "logs:GetQueryResults"
+    ]
+    resources = [
+      "arn:aws:logs:${var.region}:${var.account_id}:log-group:*"
+    ]
+  }
+  statement {
+    effect = "Allow"
+    actions = [
+      "logs:DescribeLogGroups"
+    ]
+    resources = [
+      "*"
+    ]
+  }
+  statement {
+    effect = "Allow"
+    actions = [
+      "cloudwatch:PutMetricData"
+    ]
+    resources = [
+      "*"
+    ]
+  }
+  statement {
+    effect = "Allow"
+    actions = [
+      "sqs:ChangeMessageVisibility",
+      "sqs:DeleteMessage",
+      "sqs:GetQueueAttributes",
+      "sqs:GetQueueUrl",
+      "sqs:ReceiveMessage",
+      "sqs:SendMessage"
+    ]
+    resources = [
+      "arn:aws:sqs:${var.region}:*:airflow-celery-*"
+    ]
+  }
+  statement {
+    effect = "Allow"
+    actions = [
+      "kms:Decrypt",
+      "kms:DescribeKey",
+      "kms:GenerateDataKey*",
+      "kms:Encrypt"
+    ]
+    resources = [
+      "arn:aws:kms:${var.region}::*"
+    ]
+  }
+}
+
+data "aws_iam_policy_document" "this" {
+  source_policy_documents = [
+    data.aws_iam_policy_document.base.json
+  ]
+}
